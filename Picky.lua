5ddab93bcc1ad2ab528f4223ece6595d2a415eb3
Sydbal <dominic.balassone@gmail.com>
2025-02-03 20:08:38 -0800

    added profession objects for more dynamic access and extensability

--- Picky.lua
+++ Picky.lua
@@ -12,50 +12,367 @@
 ------------------------------------------------
 -- Picky.lua (Main File)
 
--- Mining skill level requirements
-local MiningNodes = {
-	["Copper Vein"] = 1,
-	["Tin Vein"] = 65,
-	["Silver Vein"] = 75,
-	["Iron Deposit"] = 125,
-	["Gold Vein"] = 155,
-	["Mithril Deposit"] = 175,
-	["Truesilver Deposit"] = 230,
-	["Small Thorium Vein"] = 250,
-	["Rich Thorium Vein"] = 275,
-	["Ooze Covered Thorium Vein"] = 245
+-- Profession Information
+local Herbalism = {
+	nodes = {
+		["Peacebloom"] = {
+			skill = {
+				orange = 1,
+				yellow = 25,
+				green  = 50,
+				gray   = 100
+			}
+		},
+		["Silverleaf"] = {
+			skill = {
+				orange = 1,
+				yellow = 25,
+				green  = 50,
+				gray   = 100
+			}
+		},
+		["Earthroot"] = {
+			skill = {
+				orange = 15,
+				yellow = 40,
+				green  = 65,
+				gray   = 115
+			}
+		},
+		["Mageroyal"] = {
+			skill = {
+				orange = 50,
+				yellow = 75,
+				green  = 100,
+				gray   = 150
+			}
+		},
+		["Briarthorn"] = {
+			skill = {
+				orange = 70,
+				yellow = 95,
+				green  = 120,
+				gray   = 170
+			}
+		},
+		["Stranglekelp"] = {
+			skill = {
+				orange = 85,
+				yellow = 110,
+				green  = 135,
+				gray   = 185
+			}
+		},
+		["Bruiseweed"] = {
+			skill = {
+				orange = 100,
+				yellow = 125,
+				green  = 150,
+				gray   = 200
+			}
+		},
+		["Wild Steelbloom"] = {
+			skill = {
+				orange = 115,
+				yellow = 140,
+				green  = 165,
+				gray   = 215
+			}
+		},
+		["Grave Moss"] = {
+			skill = {
+				orange = 120,
+				yellow = 150,
+				green  = 170,
+				gray   = 220
+			}
+		},
+		["Kingsblood"] = {
+			skill = {
+				orange = 125,
+				yellow = 155,
+				green  = 175,
+				gray   = 225
+			}
+		},
+		["Liferoot"] = {
+			skill = {
+				orange = 150,
+				yellow = 175,
+				green  = 200,
+				gray   = 250
+			}
+		},
+		["Fadeleaf"] = {
+			skill = {
+				orange = 160,
+				yellow = 185,
+				green  = 210,
+				gray   = 260
+			}
+		},
+		["Goldthorn"] = {
+			skill = {
+				orange = 160,
+				yellow = 185,
+				green  = 210,
+				gray   = 260
+			}
+		},
+		["Khadgar's Whisker"] = {
+			skill = {
+				orange = 185,
+				yellow = 210,
+				green  = 235,
+				gray   = 285
+			}
+		},
+		["Wintersbite"] = {
+			skill = {
+				orange = 195,
+				yellow = 225,
+				green  = 245,
+				gray   = 295
+			}
+		},
+		["Firebloom"] = {
+			skill = {
+				orange = 205,
+				yellow = 235,
+				green  = 255,
+				gray   = 305
+			}
+		},
+		["Purple Lotus"] = {
+			skill = {
+				orange = 210,
+				yellow = 235,
+				green  = 260,
+				gray   = 310
+			}
+		},
+		["Arthas' Tears"] = {
+			skill = {
+				orange = 220,
+				yellow = 250,
+				green  = 270,
+				gray   = 320
+			}
+		},
+		["Sungrass"] = {
+			skill = {
+				orange = 230,
+				yellow = 255,
+				green  = 280,
+				gray   = 330
+			}
+		},
+		["Blindweed"] = {
+			skill = {
+				orange = 235,
+				yellow = 260,
+				green  = 285,
+				gray   = 335
+			}
+		},
+		["Ghost Mushroom"] = {
+			skill = {
+				orange = 245,
+				yellow = 270,
+				green  = 295,
+				gray   = 345
+			}
+		},
+		["Gromsblood"] = {
+			skill = {
+				orange = 250,
+				yellow = 275,
+				green  = 300,
+				gray   = 350
+			}
+		},
+		["Golden Sansam"] = {
+			skill = {
+				orange = 260,
+				yellow = 280,
+				green  = 310,
+				gray   = 360
+			}
+		},
+		["Dreamfoil"] = {
+			skill = {
+				orange = 270,
+				yellow = 295,
+				green  = 320,
+				gray   = 370
+			}
+		},
+		["Mountain Silversage"] = {
+			skill = {
+				orange = 280,
+				yellow = 305,
+				green  = 330,
+				gray   = 380
+			}
+		},
+		["Plaguebloom"] = {
+			skill = {
+				orange = 285,
+				yellow = 310,
+				green  = 335,
+				gray   = 385
+			}
+		},
+		["Icecap"] = {
+			skill = {
+				orange = 290,
+				yellow = 315,
+				green  = 340,
+				gray   = 390
+			}
+		},
+		["Black Lotus"] = {
+			skill = {
+				orange = 300,
+				yellow = 345,
+				green  = 375,
+				gray   = 400
+			}
+		}
+	}
 }
 
--- Herbalism skill level requirements
-local HerbNodes = {
-	["Peacebloom"] = 1,
-	["Silverleaf"] = 1,
-	["Earthroot"] = 15,
-	["Mageroyal"] = 50,
-	["Briarthorn"] = 70,
-	["Stranglekelp"] = 85,
-	["Bruiseweed"] = 100,
-	["Wild Steelbloom"] = 115,
-	["Grave Moss"] = 120,
-	["Kingsblood"] = 125,
-	["Liferoot"] = 150,
-	["Fadeleaf"] = 160,
-	["Goldthorn"] = 170,
-	["Khadgar's Whisker"] = 185,
-	["Wintersbite"] = 195,
-	["Firebloom"] = 205,
-	["Purple Lotus"] = 210,
-	["Wildvine"] = 210,
-	["Sungrass"] = 230,
-	["Blindweed"] = 235,
-	["Ghost Mushroom"] = 245,
-	["Gromsblood"] = 250,
-	["Golden Sansam"] = 260,
-	["Dreamfoil"] = 270,
-	["Mountain Silversage"] = 280,
-	["Plaguebloom"] = 285,
-	["Icecap"] = 290,
-	["Black Lotus"] = 300
+local Mining = {
+	nodes = {
+		["Copper Vein"] = {
+			skill = {
+				orange = 0,
+				yellow = 25,
+				green  = 50,
+				gray   = 100
+			}
+		},
+		["Tin Vein"] = {
+			skill = {
+				orange = 65,
+				yellow = 90,
+				green  = 115,
+				gray   = 165
+			}
+		},
+		["Silver Vein"] = {
+			skill = {
+				orange = 75,
+				yellow = 100,
+				green  = 125,
+				gray   = 175
+			}
+		},
+		["Iron Deposit"] = {
+			skill = {
+				orange = 125,
+				yellow = 150,
+				green  = 175,
+				gray   = 225
+			}
+		},
+		["Gold Vein"] = {
+			skill = {
+				orange = 155,
+				yellow = 180,
+				green  = 205,
+				gray   = 255
+			}
+		},
+		["Mithril Deposit"] = {
+			skill = {
+				orange = 175,
+				yellow = 200,
+				green  = 225,
+				gray   = 275
+			}
+		},
+		["Truesilver Deposit"] = {
+			skill = {
+				orange = 230,
+				yellow = 255,
+				green  = 280,
+				gray   = 330
+			}
+		},
+		["Ooze Covered Truesilver Deposit"] = {
+			skill = {
+				orange = 230,
+				yellow = 255,
+				green  = 280,
+				gray   = 330
+			}
+		},
+		["Dark Iron Deposit"] = {
+			skill = {
+				orange = 230,
+				yellow = 255,
+				green  = 280,
+				gray   = 330
+			}
+		},
+		["Small Thorium Vein"] = {
+			skill = {
+				orange = 245,
+				yellow = 270,
+				green  = 295,
+				gray   = 345
+			}
+		},
+		["Ooze Covered Thorium Vein"] = {
+			skill = {
+				orange = 245,
+				yellow = 270,
+				green  = 295,
+				gray   = 345
+			}
+		},
+		["Rich Thorium Vein"] = {
+			skill = {
+				orange = 275,
+				yellow = 300,
+				green  = 325,
+				gray   = 375
+			}
+		},
+		["Ooze Covered Rich Thorium Vein"] = {
+			skill = {
+				orange = 275,
+				yellow = 300,
+				green  = 325,
+				gray   = 375
+			}
+		},
+		["Hakkari Thorium Vein"] = {
+			skill = {
+				orange = 275,
+				yellow = 300,
+				green  = 325,
+				gray   = 375
+			}
+		},
+		["Small Obsidian Chunk"] = {
+			skill = {
+				orange = 305,
+				yellow = 330,
+				green  = 355,
+				gray   = 405
+			}
+		},
+		["Large Obsidian Chunk"] = {
+			skill = {
+				orange = 305,
+				yellow = 330,
+				green  = 355,
+				gray   = 405
+			}
+		}
+	}
 }
 
 -- Add check to make sure this isn't a unit,item or spell.
@@ -104,13 +421,13 @@ local function ModifyTooltip()
 	local playerSkill
 
 	-- Determine if the tooltip is for an herb or mining node
-	if MiningNodes[tooltipText] then
-		requiredSkill = MiningNodes[tooltipText]
+	if Mining.nodes[tooltipText] then
+		requiredSkill = Mining.nodes[tooltipText]["orange"]
 		playerSkill = GetProfessionSkillLevel("Mining")
 
-	elseif HerbNodes[tooltipText] then
-		requiredSkill = HerbNodes[tooltipText]
-		playerSkill = GetHerbSkillLevel("Herbalism")
+	elseif Herbalism.nodes[tooltipText] then
+		requiredSkill = Herbalism.nodes[tooltipText].skill["orange"]
+		playerSkill = GetProfessionSkillLevel("Herbalism")
 
 	end
 
---
+++ ProfessionInfo/Herbalism.lua
@@ -0,0 +1,228 @@
+Herbalism = {
+	nodes = {
+		["Peacebloom"] = {
+			skill = {
+				orange = 1,
+				yellow = 25,
+				green  = 50,
+				gray   = 100
+			}
+		},
+		["Silverleaf"] = {
+			skill = {
+				orange = 1,
+				yellow = 25,
+				green  = 50,
+				gray   = 100
+			}
+		},
+		["Earthroot"] = {
+			skill = {
+				orange = 15,
+				yellow = 40,
+				green  = 65,
+				gray   = 115
+			}
+		},
+		["Mageroyal"] = {
+			skill = {
+				orange = 50,
+				yellow = 75,
+				green  = 100,
+				gray   = 150
+			}
+		},
+		["Briarthorn"] = {
+			skill = {
+				orange = 70,
+				yellow = 95,
+				green  = 120,
+				gray   = 170
+			}
+		},
+		["Stranglekelp"] = {
+			skill = {
+				orange = 85,
+				yellow = 110,
+				green  = 135,
+				gray   = 185
+			}
+		},
+		["Bruiseweed"] = {
+			skill = {
+				orange = 100,
+				yellow = 125,
+				green  = 150,
+				gray   = 200
+			}
+		},
+		["Wild Steelbloom"] = {
+			skill = {
+				orange = 115,
+				yellow = 140,
+				green  = 165,
+				gray   = 215
+			}
+		},
+		["Grave Moss"] = {
+			skill = {
+				orange = 120,
+				yellow = 150,
+				green  = 170,
+				gray   = 220
+			}
+		},
+		["Kingsblood"] = {
+			skill = {
+				orange = 125,
+				yellow = 155,
+				green  = 175,
+				gray   = 225
+			}
+		},
+		["Liferoot"] = {
+			skill = {
+				orange = 150,
+				yellow = 175,
+				green  = 200,
+				gray   = 250
+			}
+		},
+		["Fadeleaf"] = {
+			skill = {
+				orange = 160,
+				yellow = 185,
+				green  = 210,
+				gray   = 260
+			}
+		},
+		["Goldthorn"] = {
+			skill = {
+				orange = 160,
+				yellow = 185,
+				green  = 210,
+				gray   = 260
+			}
+		},
+		["Khadgar's Whisker"] = {
+			skill = {
+				orange = 185,
+				yellow = 210,
+				green  = 235,
+				gray   = 285
+			}
+		},
+		["Wintersbite"] = {
+			skill = {
+				orange = 195,
+				yellow = 225,
+				green  = 245,
+				gray   = 295
+			}
+		},
+		["Firebloom"] = {
+			skill = {
+				orange = 205,
+				yellow = 235,
+				green  = 255,
+				gray   = 305
+			}
+		},
+		["Purple Lotus"] = {
+			skill = {
+				orange = 210,
+				yellow = 235,
+				green  = 260,
+				gray   = 310
+			}
+		},
+		["Arthas' Tears"] = {
+			skill = {
+				orange = 220,
+				yellow = 250,
+				green  = 270,
+				gray   = 320
+			}
+		},
+		["Sungrass"] = {
+			skill = {
+				orange = 230,
+				yellow = 255,
+				green  = 280,
+				gray   = 330
+			}
+		},
+		["Blindweed"] = {
+			skill = {
+				orange = 235,
+				yellow = 260,
+				green  = 285,
+				gray   = 335
+			}
+		},
+		["Ghost Mushroom"] = {
+			skill = {
+				orange = 245,
+				yellow = 270,
+				green  = 295,
+				gray   = 345
+			}
+		},
+		["Gromsblood"] = {
+			skill = {
+				orange = 250,
+				yellow = 275,
+				green  = 300,
+				gray   = 350
+			}
+		},
+		["Golden Sansam"] = {
+			skill = {
+				orange = 260,
+				yellow = 280,
+				green  = 310,
+				gray   = 360
+			}
+		},
+		["Dreamfoil"] = {
+			skill = {
+				orange = 270,
+				yellow = 295,
+				green  = 320,
+				gray   = 370
+			}
+		},
+		["Mountain Silversage"] = {
+			skill = {
+				orange = 280,
+				yellow = 305,
+				green  = 330,
+				gray   = 380
+			}
+		},
+		["Plaguebloom"] = {
+			skill = {
+				orange = 285,
+				yellow = 310,
+				green  = 335,
+				gray   = 385
+			}
+		},
+		["Icecap"] = {
+			skill = {
+				orange = 290,
+				yellow = 315,
+				green  = 340,
+				gray   = 390
+			}
+		},
+		["Black Lotus"] = {
+			skill = {
+				orange = 300,
+				yellow = 345,
+				green  = 375,
+				gray   = 400
+			}
+		}
+	}
+}
